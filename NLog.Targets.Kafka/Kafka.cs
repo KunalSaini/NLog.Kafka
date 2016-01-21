315d338d6c1dc9fd20cf82968a857244b3ae09a6
Kunal Saini <Kunal.Saini@makemytrip.com>
2016-01-22 00:08:58 +0530

    Initial commit

---
+++ NLog.Targets.Kafka/Kafka.cs
@@ -0,0 +1,28 @@
+﻿using NLog.Common;
+using NLog.Config;
+
+namespace NLog.Targets
+{
+    [Target("Kafka")]
+    public class Kafka : TargetWithLayout
+    {
+        public Kafka()
+        {
+            this.Host = "localhost";
+        }
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var message = this.Layout.Render(logEvent);
+            SendMessageToQueue(message);
+            base.Write(logEvent);
+        }
+
+        private void SendMessageToQueue(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [RequiredParameter]
+        public string Host { get; set; }
+    }
+}
