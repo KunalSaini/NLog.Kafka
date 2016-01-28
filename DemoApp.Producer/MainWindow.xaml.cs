0951467318acce0547612fe09b2b2107ccdb8ee9
Kunal Saini <Kunal8484@yahoo.co.in>
2016-01-28 23:54:02 +0530

    Demo app various button handling

--- DemoApp.Producer/MainWindow.xaml.cs
+++ DemoApp.Producer/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using NLog;
 using NLog.Targets;
 using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DemoApp.Producer
 {
@@ -10,18 +12,42 @@ namespace DemoApp.Producer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private static Logger logger;
+        
+        static MainWindow()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+        }
 
         public MainWindow()
         {
             InitializeComponent();
         }
-        //KafkaClient kafkaClient = new KafkaClient();
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //kafkaClient.Post("Hello World at - " + DateTime.Now.Ticks);
-            logger.Trace("This is a sample trace message " + DateTime.Now.Ticks);
+            var b = (Button)sender;
+            switch (b.Name)
+            {
+                case "Trace":
+                    logger.Trace("This is a sample trace message");
+                    break;
+                case "Debug":
+                    logger.Debug("This is a sample debug message", "important");
+                    break;
+                case "Warn":
+                    logger.Warn("This is a sample warn message");
+                    break;
+                case "Error":
+                    logger.Error("This is a sample error message");
+                    break;
+                case "Fatal":
+                    logger.Fatal("This is a sample fatal message");
+                    break;
+                default:
+                    logger.Info("This is a sample default info message");
+                    break;
+            }
         }
     }
 }
