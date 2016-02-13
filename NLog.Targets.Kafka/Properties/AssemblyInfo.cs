20c30216130fae9cd2e82d193dc8a6a9aea87d40
Kunal Saini <Kunal8484@yahoo.co.in>
2016-02-14 00:35:50 +0530

    Basic Nuget

--- NLog.Targets.Kafka/Properties/AssemblyInfo.cs
+++ NLog.Targets.Kafka/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLog.Targets.Kafka")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NLog.Kafka")]
+[assembly: AssemblyDescription("The NLog target to write to Kafka queue")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("kunal")]
 [assembly: AssemblyProduct("NLog.Targets.Kafka")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1-rc")]
